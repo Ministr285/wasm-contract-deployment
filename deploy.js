230b43beb608bd8578f24d44769eb0a6eca4058a
Ministr285 <andruscavictor32@gmail.com>
2023-02-18 18:15:09 +0300

    Revert "Update deploy.js"
    This reverts commit 719f8c94940a1d045d7bbbd7572f33fb859eff22.

--- deploy.js
+++ deploy.js
@@ -13,7 +13,7 @@ try {
   let address; // variable for storing the address of the deployed contract 
 
   // API creation for connection to the chain
-  const wsProvider = new WsProvider('wss://wss-testnet.5ire.network/');
+  const wsProvider = new WsProvider('ws://127.0.0.1:9944');
   const api = await ApiPromise.create({ provider: wsProvider });
 
   
@@ -31,7 +31,7 @@ try {
   
   
   // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
+  const PHRASE = 'solar avocado consider wide hurdle civil message curious despair bicycle sleep live';
   const seedUser = mnemonicToMiniSecret(PHRASE);
   const keyring = new Keyring({ type: "ed25519" });
   const userKeyring = keyring.addFromPair(naclKeypairFromSeed(seedUser));
